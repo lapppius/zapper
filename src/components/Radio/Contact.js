7fe6714c8bf2f74a6663c4293caebe2a1e4204d6
ioannislappas <73820845+ioannislappas@users.noreply.github.com>
2022-03-22 02:39:42 +0200

    style

--- src/components/Radio/Contact.js
+++ src/components/Radio/Contact.js
@@ -26,11 +26,10 @@ export default function Contact(props) {
 
     const loc = (
         <>
-            {' '}
-            <p>{city != null ? city + ', ' : ''}</p>
-            <p>{street != null ? street : ''}</p>
-            <p>{number != null ? number + ', ' : ''}</p>
-            <p>{postcode != null ? postcode : ''}</p>
+            {city != undefined ? city : null}
+            {street != undefined ? ' ' + street : null}
+            {number != null ? ' ' + number : null}
+            {postcode != undefined ? ' ' + postcode : null}
         </>
     );
     return (
@@ -41,7 +40,6 @@ export default function Contact(props) {
                         <span>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
-                                height="24px"
                                 viewBox="0 0 24 24"
                                 width="24px"
                                 height="24px"
@@ -58,15 +56,12 @@ export default function Contact(props) {
                             {props.email.replace('mailto:', '')}
                         </a>
                     </li>
-                ) : (
-                    ''
-                )}
+                ) : null}
                 {props.phone ? (
                     <li>
                         <span>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
-                                height="24px"
                                 viewBox="0 0 24 24"
                                 width="24px"
                                 height="24px"
@@ -93,9 +88,7 @@ export default function Contact(props) {
                             ))}
                         </ul>
                     </li>
-                ) : (
-                    ''
-                )}
+                ) : null}
                 {street || number || postcode || city ? (
                     <li>
                         <span>
@@ -105,7 +98,6 @@ export default function Contact(props) {
                                 height="24px"
                                 viewBox="0 0 24 24"
                                 width="24px"
-                                height="24px"
                             >
                                 <path d="M12,2c-4.2,0-8,3.22-8,8.2c0,3.18,2.45,6.92,7.34,11.23c0.38,0.33,0.95,0.33,1.33,0C17.55,17.12,20,13.38,20,10.2 C20,5.22,16.2,2,12,2z M12,12c-1.1,0-2-0.9-2-2c0-1.1,0.9-2,2-2c1.1,0,2,0.9,2,2C14,11.1,13.1,12,12,12z" />
                             </svg>
