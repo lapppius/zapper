2840f656ea85721dfefec74a6f2ac6eeaaef2151
ioannislappas <73820845+ioannislappas@users.noreply.github.com>
2023-10-17 14:33:21 +0300

    clean up

--- src/pages/Radios.jsx
+++ src/pages/Radios.jsx
@@ -3,16 +3,12 @@ import { useEffect, useState } from 'react';
 import { getWikidataEntitiesPromise } from '../FetchFunctions';
 
 export default function Radios() {
-    const radiosSession = sessionStorage.getItem('radiosListSes');
     const [isLoading, setIsLoading] = useState(true);
     const [loadedRadios, setLoadedRadios] = useState([]);
-    // const test1 = sessionStorage.getItem('test1');
 
     useEffect(() => {
-        if (radiosSession === null) {
             setIsLoading(true);
             getWikidataEntitiesPromise().then((response) => {
-                console.log(response);
                 const radios = [];
                 for (const obj of Object.entries(response.entities)) {
                     try {
@@ -27,24 +23,14 @@ export default function Radios() {
                         radio.streamUrl =
                             obj[1].claims.P963[0].mainsnak.datavalue.value;
 
-                        console.log(radio.title);
                         radios.push(radio);
                     } catch (error) {}
                 }
                 setIsLoading(false);
                 setLoadedRadios(radios);
-                sessionStorage.setItem('radiosListSes', JSON.stringify(radios));
             });
-        } else {
-            setIsLoading(false);
-            setLoadedRadios(JSON.parse(radiosSession));
-        }
-        // if (test1 == null) {
-        //     console.log('null');
-        // } else {
-        //     console.log(test1);
-        // }
-    }, [radiosSession]);
+ 
+    }, []);
 
     return <>{isLoading ? undefined : <RadiosList radios={loadedRadios} />}</>;
 }
