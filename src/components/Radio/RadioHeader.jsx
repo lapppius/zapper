92688a5d8bc0b21e85e7855a9fa593b89dc05cf6
ioannislappas <73820845+ioannislappas@users.noreply.github.com>
2023-10-19 20:03:43 +0300

    skeleton

--- src/components/Radio/RadioHeader.jsx
+++ src/components/Radio/RadioHeader.jsx
@@ -6,6 +6,8 @@ import * as Vibrant from "node-vibrant";
 import { useEffect, useRef, useState, useContext } from "react";
 import { fetchWikiSummary, setImagesListPromise } from "../../FetchFunctions";
 import { PlayerContext } from "../../App";
+import { Typography, Skeleton } from "@mui/material";
+import React from "react";
 
 export default function RadioHeader(props) {
   const playerContext = useContext(PlayerContext);
@@ -60,7 +62,7 @@ export default function RadioHeader(props) {
               },
             }));
       }
-    })
+    });
   }, [wikiSummaryRef, props.title]);
 
   useEffect(() => {
@@ -95,7 +97,21 @@ export default function RadioHeader(props) {
     >
       <header>
         <div className={styles.headerText}>
-          <h1>{props.title}</h1>
+          <h1>
+            {!props.title ? (
+              <Skeleton
+                sx={{
+                  bgcolor: "grey.900",
+                  variant: "rounded",
+                  height: "2rem",
+                  width: "13rem",
+                  animation: "wave",
+                }}
+              />
+            ) : (
+              props.title
+            )}
+          </h1>
         </div>
 
         <RadioImg
@@ -121,7 +137,30 @@ export default function RadioHeader(props) {
           <ShareButton {...props} />
         </div>
         <p className={styles.radioDescription} ref={wikiSummaryRef}>
-          {wikiSummary.wikiSum.sumContent}
+          {wikiSummary.wikiSum.sumContent ? (
+            wikiSummary.wikiSum.sumContent
+          ) : (
+            <React.Fragment>
+              <Skeleton
+                sx={{
+                  bgcolor: "grey.900",
+                  variant: "rounded",
+                  height: "1.5rem",
+                  width: "22rem",
+                  animation: "wave",
+                }}
+              />
+              <Skeleton
+                sx={{
+                  bgcolor: "grey.900",
+                  variant: "rounded",
+                  height: "1.5rem",
+                  width: "13rem",
+                  animation: "wave",
+                }}
+              />
+            </React.Fragment>
+          )}
           {wikiSummary.wikiSum.isWiki == undefined ? (
             ""
           ) : wikiSummary.wikiSum.isWiki ? (
