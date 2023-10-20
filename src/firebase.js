0ca1061c7fbcfc5176b2debb1cd43bf16daa671d
lapppius <73820845+lapppius@users.noreply.github.com>
2023-10-20 11:18:21 +0300

    Revert "."
    This reverts commit c9f305b0442502d5639094aef0abb50257cd6fb2.

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+import { useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getAuth, updateCurrentUser, updateProfile } from "firebase/auth";
 import {
